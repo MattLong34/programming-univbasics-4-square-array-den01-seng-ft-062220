bb0c9eea1ab3d24547f9d8684f89f2d970ca1e2c
Matt Long <mdlong34@gmail.com>
2020-05-29 15:39:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,16 @@
 def square_array(array)
-  while array.length.times do
-    array**2
+  count = 0 
+  while count < array.length do
+    puts array.to_i**
+    count = count +1 
   end
 end
+
+
+
+
+
+
 
 
 # array = ["car",2,"tree",4,5.4]
@@ -10,9 +18,3 @@ end
 # array.length.times do |index|
 #   puts array[index]
 # end
-
-count = 0 
-while count < array.length do
-  puts array.to_i**
-  count = count +1 
-end
