844f072c0d9ac1fd2e680d476a48a80405ca83d3
Matt Long <mdlong34@gmail.com>
2020-05-29 15:36:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,12 @@
 def square_array(array)
-  while times.array.length do
+  while array.length.times do
     array**2
   end
+end
+
+
+# array = ["car",2,"tree",4,5.4]
+ 
+array.length.times do |index|
+  puts array[index]
 end
