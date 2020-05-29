ef2dbfe4024278f86eb6d94499009ecbcbbf393e
Matt Long <mdlong34@gmail.com>
2020-05-29 15:47:03 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -8,14 +8,3 @@ def square_array(array)
   new_array
 end
 
-
-
-# def square_array(array)
-#   count = 0
-#   	sq_arr = []
-#   	while count < array.length do
-#     	sq_arr.push(array[count] ** 2)
-#     	count += 1
-#   	end
-#   	sq_arr
-# end
