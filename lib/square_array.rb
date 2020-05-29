6145dcad1809fd5510350e30b3c572f18f1bc826
Matt Long <mdlong34@gmail.com>
2020-05-29 15:38:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -7,12 +7,12 @@ end
 
 # array = ["car",2,"tree",4,5.4]
  
-array.length.times do |index|
-  puts array[index]
-end
+# array.length.times do |index|
+#   puts array[index]
+# end
 
 count = 0 
-while count < 3 do
-  puts "#{count}"
+while count < array.length do
+  puts array.to_i**
   count = count +1 
 end
